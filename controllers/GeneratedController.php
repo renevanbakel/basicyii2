b96d9e567f2dd276b82a60f1e3318925a83c81eb
Rene van Bakel <renevanbakel@gmail.com>
2025-12-10 14:54:42 +0100

    New controller via Gii

---
+++ controllers/GeneratedController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+class GeneratedController extends \yii\web\Controller
+{
+    public function actionGetOrderByOrderId()
+    {
+        return $this->render('get-order-by-order-id');
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionShowOrderByOrderId()
+    {
+        return $this->render('show-order-by-order-id');
+    }
+
+}
